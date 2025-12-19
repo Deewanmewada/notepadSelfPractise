7bd3f4fd3d487d5c93941b574f17c072f57a192b
Deewan Mewada <deewanmewada81@gmail.com>
2025-12-19 13:17:49 +0530

    adding file from hell to cmdArgs

---
+++ cmdArgs.java
@@ -0,0 +1,11 @@
+public class cmdArgs {
+	public static void main(String [] args){
+		String name = args[0];
+		String location = args[1];
+	
+		
+		System.out.println("Welcome to  the " + name);
+		System.out.println(name + " is located in " + location);
+
+	}
+}
---
+++ cmdArgs1.java
@@ -0,0 +1,9 @@
+public class cmdArgs1{
+	public static void main(String[] args){
+		String mentor = args[0];
+		int experience = Integer.parseInt(args[1]);
+		
+		System.out.println(mentor + " sir");
+		System.out.println(experience + " years");
+	}
+}
