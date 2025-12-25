148717b4ec96ac01d2b57d27d3456f4142495894
Deewan Mewada <deewanmewada81@gmail.com>
2025-12-25 19:01:22 +0530

    Added new file in HowToCallMethod folder

---
+++ HowToCallMethod/AreaOfReactangle.java
@@ -0,0 +1,13 @@
+// area of reactangle
+
+public class AreaOfReactangle{
+
+public static int areaOfReactangle(int length, int width ){
+	return length * width;
+}
+public static void main(String [] args){
+	int res = AreaOfReactangle.areaOfReactangle(5,5);
+
+	System.out.println("area of reactangle is : " + res);
+	}
+}
